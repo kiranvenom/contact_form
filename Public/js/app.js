cf7cc2b7c3249118b946f2a261b5d6392628e372
Kiran <km060360@gmail.com>
2023-09-12 11:31:08 +0530

    fully working

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,21 +1,38 @@
-const contaccrForm = document.querySelector('.contact-form');
+const contactForm = document.querySelector('.contact-form');
 const fullName = document.getElementById('name');
 const mail = document.getElementById('mail');
 const subject = document.getElementById('subject');
 const message = document.getElementById('message');
+const sendMessageButton = document.getElementById('sendMessage');
 
-contaccrForm.addEventListener('submit', (e) => {
+let isSending = false;
+
+contactForm.addEventListener('submit', (e) => {
 	e.preventDefault();
+
+	if (isSending) {
+		return;
+	}
+
+	isSending = true;
+	sendMessageButton.disabled = true;
+	sendMessageButton.style.cursor = 'not-allowed';
+
 	let formData = {
 		fullName: fullName.value,
 		mail: mail.value,
 		subject: subject.value,
 		message: message.value,
 	};
+
 	let xhr = new XMLHttpRequest();
 	xhr.open('POST', '/');
 	xhr.setRequestHeader('content-type', 'application/json');
 	xhr.onload = function () {
+		isSending = false;
+		sendMessageButton.disabled = false;
+		sendMessageButton.style.cursor = 'pointer'; 
+
 		console.log(xhr.responseText);
 		if (xhr.responseText == 'success') {
 			alert('email sent');
